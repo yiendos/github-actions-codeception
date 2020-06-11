dc221f1404800dd6c456fa6f20297c02a10b39c2
yiendos <yiendos@sbmatrix.com>
2020-06-11 16:25:56 +0100

    #master: whoops duplicating steps in bootstrap.sh

--- docker/scripts/bootstrap.sh
+++ docker/scripts/bootstrap.sh
@@ -2,28 +2,7 @@
 
 set -e
 
-echo "Update installable packages"
-
-apt update -y
-
-echo "Install necessary stuff to create a new joomla site"
-
-apt install curl git unzip -y
-
-echo "Now we are going to get composer"
-curl -sS https://getcomposer.org/installer -o /root/composer-setup.php
-
-echo "Move to local bin"
-
-php /root/composer-setup.php --install-dir=/usr/local/bin --filename=composer
-
-echo "Make path executable"
-
 export PATH=/root/.composer/vendor/bin/:$PATH >> ~/.bashrc
-
-echo "Composer install joomlatools-console"
-
-composer global require joomlatools/console --no-interaction
 
 echo "Download site"
 joomla site:download testing
