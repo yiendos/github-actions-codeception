83c968b5e59e900325f919b41972326e99852519
yiendos <yiendos@sbmatrix.com>
2020-06-29 12:41:58 +0100

    #2: editable, but db install and configuration seems to take an age

--- docker/scripts/example-bootstrap.sh
+++ docker/scripts/example-bootstrap.sh
@@ -6,8 +6,12 @@ export PATH=/root/.composer/vendor/bin/:$PATH >> ~/.bashrc
 
 echo "Download site"
 
-joomla site:download testing --release=4.0.0-beta
+joomla site:download testing --release=4.0.0-beta --www=/tmp
 
-rm -rf /var/www/testing/administrator/templates/atum/css; rm -rf /var/www/testing/templates/cassiopeia/css; rm -rf /var/www/testing/media/; rm -rf /var/www/testing/node_modules/; rm -rf /var/www/testing/libraries/vendor/;rm -f /var/www/testing/administrator/cache/autoload_psr4.php; rm -rf /var/www/testing/installation/template/css;
+chown -R www-data:www-data /tmp/testing
 
-composer install --working-dir=/var/www/testing/ --ignore-platform-reqs; npm ci --prefix /var/www/testing/ --unsafe-perm;
+rm -rf /tmp/testing/administrator/templates/atum/css; rm -rf /tmp/testing/templates/cassiopeia/css; rm -rf /tmp/testing/media/; rm -rf /tmp/testing/node_modules/; rm -rf /tmp/testing/libraries/vendor/;rm -f /tmp/testing/administrator/cache/autoload_psr4.php; rm -rf /tmp/testing/installation/template/css;
+
+composer install --working-dir=/tmp/testing/ --ignore-platform-reqs; npm ci --prefix /tmp/testing/ --unsafe-perm;
+
+mkdir -p /var/www/testing
--- docker/scripts/install-database.sh
+++ docker/scripts/install-database.sh
@@ -8,6 +8,8 @@ sleep 10
 
 export PATH=/root/.composer/vendor/bin/:$PATH >> ~/.bashrc
 
+cp -RP /tmp/testing/* /var/www/testing/
+
 joomla database:install testing --mysql-host=db --mysql-login=joomla:joomla --sql-dumps=/var/www/testing/installation/sql/mysql/base.sql --sql-dumps=/var/www/testing/installation/sql/mysql/extensions.sql --sql-dumps=/var/www/testing/installation/sql/mysql/supports.sql --sql-dumps=/tmp/joomla_beta_users.sql --drop;
 
 joomla site:configure testing --mysql-host=db --mysql-login=joomla:joomla;
